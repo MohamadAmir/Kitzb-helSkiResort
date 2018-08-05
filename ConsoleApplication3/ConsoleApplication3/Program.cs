f7e557d51c7f033fd30f79e527ab47d368d7a1de
MohamadAmir <42119714+MohamadAmir@users.noreply.github.com>
2018-08-05 20:41:42 +0200

    modify paths class

--- ConsoleApplication3/ConsoleApplication3/Program.cs
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -9,44 +9,52 @@ namespace ConsoleApplication3
 {
     class Program
     {
+        public class location
+        {
+            public int x;
+            public int y;
+            public int height;
+        }
+        public class pathes
+        {
+            public Stack<location> path = new Stack<location>();
+        }
+
         private static location[,] map; // matrix data is stored here
         private static int row; // the row length of the matrix
         private static int column; // the column length of the matrix
-        public class location { public int x; public int y; public int height; }
-        public class pathes { public int[] pathLength=new int[3];public Stack<location> path = new Stack<location>(); } 
-        public static pathes getLongPath(int i, int j)
+        
+        
+        public static pathes getLongestPath(int i, int j)
         {
             pathes p = new pathes();
             pathes curPath = new pathes();
             if (j > 0 && map[i,j].height > map[i,j - 1].height)// search left direction
             {
-                curPath = getLongPath(i, j - 1);
-                if (curPath.pathLength[0] > p.pathLength[0]) // if the current path length is bigger
+                curPath = getLongestPath(i, j - 1);
+                if (curPath.path.Count> p.path.Count) // if the current path length is bigger
                     p = curPath;
             }
             if (j < (column - 1) && map[i,j].height > map[i,j + 1].height)// search right direction
             {
-                curPath = getLongPath(i, j + 1);
-                if (curPath.pathLength[0] > p.pathLength[0])
+                curPath = getLongestPath(i, j + 1);
+                if (curPath.path.Count > p.path.Count)
                     p = curPath;
             }
-            // search from the left direction
             if (i > 0 && map[i,j].height > map[i - 1,j].height)// search Up direction
             {
-                curPath = getLongPath(i - 1, j);
-                if (curPath.pathLength[0] > p.pathLength[0])
+                curPath = getLongestPath(i - 1, j);
+                if (curPath.path.Count > p.path.Count)
                     p = curPath;
             }
             if (i < (row - 1) && map[i,j].height > map[i + 1,j].height)// search down direction
             {
-                curPath = getLongPath(i + 1, j);
-                if (curPath.pathLength[0] > p.pathLength[0])
+                curPath = getLongestPath(i + 1, j);
+                if (curPath.path.Count > p.path.Count)
                 {
-                    p = curPath;
-                    
+                    p = curPath;       
                 }
             }
-            p.pathLength[0]++;// add 1 to the path length
             location l = new location();
             l.x = i;
             l.y = j;
@@ -55,44 +63,52 @@ namespace ConsoleApplication3
             return p;
         }
 
-        static void Main(string[] args)
+        public static void loadMapFromFile(string filePath)//load file data to Map
         {
-            string[] lines = File.ReadAllLines(@"d:\map.txt");
-            column = Convert.ToInt32(lines[0].Split(' ')[0]);
-            row = Convert.ToInt32(lines[0].Split(' ')[1]);
-            
-            map = new location[column,row];
-            for (int i = 1; i < lines.Length; i++)
+            try
             {
-                string[] mapVerticalLocations = lines[i].Split(' ');
-                for (int j = 0; j < mapVerticalLocations.Length; j++)
+                string[] lines = File.ReadAllLines(filePath);
+                column = Convert.ToInt32(lines[0].Split(' ')[0]);
+                row = Convert.ToInt32(lines[0].Split(' ')[1]);
+
+                map = new location[column, row];
+                for (int i = 1; i < lines.Length; i++)
                 {
-                    if (mapVerticalLocations[j] != null)
+                    string[] mapVerticalLocations = lines[i].Split(' ');
+                    for (int j = 0; j < mapVerticalLocations.Length; j++)
                     {
-                        try
+                        if (mapVerticalLocations[j] != null)
                         {
-                            map[i - 1, j] = new location();
-                            map[i - 1, j].height = Convert.ToInt32(mapVerticalLocations[j]);
-                            map[i - 1, j].x = i;
-                            map[i - 1, j].y = j;
-                        }
-                        catch 
-                        {
-                            map[i - 1, j].height = -1;
-                            map[i - 1, j].x = i;
-                            map[i - 1, j].y = j;
+                            try
+                            {
+                                map[i - 1, j] = new location();
+                                map[i - 1, j].height = Convert.ToInt32(mapVerticalLocations[j]);
+                                map[i - 1, j].x = i;
+                                map[i - 1, j].y = j;
+                            }
+                            catch
+                            {
+                                map[i - 1, j].height = -1;
+                                map[i - 1, j].x = i;
+                                map[i - 1, j].y = j;
+                            }
                         }
                     }
                 }
             }
-            /*//////////////////////////////*/
+            catch (Exception e2)
+            { Console.WriteLine("An error occurred while loading file: " + e2.Message); }
+        }
+        static void Main(string[] args)
+        {
+            loadMapFromFile(@"d:\map.txt");
             pathes longestPath = new pathes();
             for (int i = 0; i < row; i++)
             {
                 for (int j = 0; j < column; j++)
                 {
 
-                    pathes temp = getLongPath(i, j);
+                    pathes temp = getLongestPath(i, j);
                     if (longestPath.path.Count == temp.path.Count)
                     {
                         if (longestPath.path.Count > 0)
@@ -113,12 +129,21 @@ namespace ConsoleApplication3
                     }
                 }
             }
-            Console.WriteLine("Maximal Path is: " + longestPath.pathLength[0]+ "\nMaximal Drop is: " + (longestPath.path.Peek().height-longestPath.path.ElementAt(longestPath.path.Count-1).height).ToString());
-            Console.WriteLine("The path is :\n");
+            Console.WriteLine("Maximal Path is: " + longestPath.path.Count.ToString()+ "\nMaximal Drop is: " + (longestPath.path.Peek().height-longestPath.path.ElementAt(longestPath.path.Count-1).height).ToString());
+            Console.WriteLine("The path is : ");
+            string printPath = "";
             for (int i = 0; i < longestPath.path.Count; i++)
             {
-                Console.WriteLine(longestPath.path.ElementAt(i).height.ToString()+ "  >==>  ");
+                if (i == longestPath.path.Count - 1)
+                {
+                    printPath += longestPath.path.ElementAt(i).height.ToString();
+                }
+                else
+                {
+                    printPath += longestPath.path.ElementAt(i).height.ToString() + "  >==>  ";
+                }
             }
+            Console.WriteLine(printPath);
             System.Console.ReadKey();
         }
     }
